c22a67885312db0de407f3f398fc82d7bab9b3dc
Scala Steward <scala-steward@ledger.com>
2021-05-24 00:06:56 +0000

    Update doobie-core, doobie-h2, ... to 0.13.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
 
   val H2Version     = "1.4.200"
   val flywayVersion = "7.8.1"
-  val doobieVersion = "0.13.1"
+  val doobieVersion = "0.13.4"
   val postgres: Seq[ModuleID] = Seq(
     "com.h2database" % "h2"              % H2Version,
     "org.flywaydb"   % "flyway-core"     % flywayVersion,
